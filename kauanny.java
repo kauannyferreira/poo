ae6fb3b40a1aa4a24d39d8d76ba7a9dbb77cc04f
kauannyferreira <135665143+kauannyferreira@users.noreply.github.com>
2023-06-05 18:19:33 -0300

    Add files via upload

---
+++ kauanny.java
@@ -0,0 +1,29 @@
+ pessoa p1 = new pessoa();
+ p1.nome = "Maria" ;
+ p1.idade = 18;
+ p1.sexo = 'f';
+ p1.cpf = "123.456.789-00";
+ 
+ pessoa p2 = new pessoa();
+ p2.nome = "joao";
+ p2.idade = 22;
+ p2.sexo = 'm';
+ p2.cpf = "234.567.890-00";
+ 
+ 
+ conta c1 = new conta();
+ c1.numero = "1234-5";
+ c1.titular = p1;
+ c1.saldo = 100.0;
+ c1.limite = 200.0;
+ 
+ conta c2 = new conta();
+ c2.numero = "2345-6";
+ c2.titular = p2;
+ c2.saldo = 150.0;
+ c2.limite = 200.0;
+ 
+ systen.out.printlm(c1.saldo);
+ systen.out.printlm(c1.titular.nome);
+ systen.out.printlm("saldo da conta " + c2.numero + ":" + c2.saldo);
+ 
